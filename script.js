d8aa7b223358df373eb25151d188de4cd0e4e6ea
ㅤBoltznn <luisfelipedamata070@gmail.com>
2026-03-22 20:18:34 -0300

    Adicionando a multiplicacao

---
+++ script.js
@@ -0,0 +1,5 @@
+let numUm = Number(prompt("Digite um número:"));
+let numDois = Number(prompt("Digite outro número:"));
+let multiplicacao = numUm * numDois;
+
+alert(`O produto entre o ${numUm} e o ${numDois} será  ${multiplicacao}`);
