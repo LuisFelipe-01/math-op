5128c05ec0c8f5caf69a615cd62a0fe766818572
pedro catatau <pedroka10kas@gmail.com>
2026-03-22 21:35:08 -0300

    Adicionando a Subtração ao repositório

---
+++ script.js
@@ -0,0 +1,5 @@
+let qtdUm = Number(prompt("Digite o primeiro número:"));
+let qtdDois = Number(prompt("Digite o segundo número:"));
+let subtracao = qtdUm - qtdDois;
+
+alert(`A subtração entre o ${qtdUm} e o ${qtdDois} é igual a ${subtracao}.`);
