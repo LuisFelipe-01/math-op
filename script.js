3c51237effd6db11d65ebcc141109225265e79db
I1HN <vitorgazola2710@gmail.com>
2026-03-25 07:49:59 -0300

    Acrescentado a Adição

---
+++ script.js
@@ -0,0 +1,5 @@
+let numUm = Number(prompt("insira o primeiro número:"));
+let numDois = Number(prompt("insira o segundo número:"));
+let adicao = numUm + numDois;
+
+alert(`A soma do ${numUm} e do ${numDois} dará o total de ${adicao.toFixed(2)}`);
