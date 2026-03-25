a2afb7684046af64d63e8bc2ce7e00b07c67b6df
I1HN <yurioruam09@gmail.com>
2026-03-25 08:06:26 -0300

    Foi adicionado divisão ao repositório

---
+++ script.js
@@ -0,0 +1,5 @@
+let valorUm = Number(prompt("Insira o primeiro valor:"));
+let valorDois = Number(prompt("Insira o segundo valor:"));
+let dvisao = valorUm / valorDois;
+
+alert(`O valor da divisão é igual a ${dvisao.toFixed(2)}`);
