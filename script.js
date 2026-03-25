02ecdd599e9f1022d9fdf42ff71ae4e8fc1067ee
I1HN <pedroka10kas@gmail.com>
2026-03-25 07:38:19 -0300

    Adicionando -toFixed- a subtração

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ let qtdUm = Number(prompt("Digite o primeiro número:"));
 let qtdDois = Number(prompt("Digite o segundo número:"));
 let subtracao = qtdUm - qtdDois;
 
-alert(`A subtração entre o ${qtdUm} e o ${qtdDois} é igual a ${subtracao}.`);
+alert(`A subtração entre o ${qtdUm} e o ${qtdDois} é igual a ${subtracao.toFixed(2)}.`);
